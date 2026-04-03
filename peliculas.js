46ded2747fe1ba6fa8669d6f276bdea97e7e75c8
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-04-03 11:17:03 -0500

    Add files via upload

---
+++ peliculas.js
@@ -0,0 +1,26 @@
+// Este es tu archivo peliculas.js
+const baseDeDatos = [
+    { 
+        id: "NGOD-170", 
+        titulo: "I Got Fingered By My Husband's Mean Boss...", 
+        poster: "https://cdn001.imggle.net/webp/poster/20221202/1649409013.webp", 
+        bg: "https://pics.dmm.co.jp/digital/video/ngod00170/ngod00170jp-13.jpg", 
+        video: "https://appcine.b-cdn.net/Codegg/NGOD-170.mp4", 
+        estudio: "Jet Eizo",
+        fecha: "Apr. 08, 2022", 
+        actriz: "Nene Tanaka",
+        tags: "Uncensored, Creampie" 
+    },
+    { 
+        id: "IPIT-012", 
+        titulo: "Business Trip Destination Room NTR Blond Female Employee Who Continued", 
+        poster: "https://cdn001.imggle.net/webp/poster/20212709/1637387937.webp", 
+        bg: "https://pics.dmm.co.jp/digital/video/ipit00012/ipit00012jp-10.jpg", 
+        video: "https://appcine.b-cdn.net/Codegg/IPIT-012.mp4", 
+        estudio: "Idea Pocket",
+        fecha: "2023-11-20",
+        actriz: "Lily Hart",
+        tags: "Older Sister"
+    }
+    // Sigue pegando aquí todos los bloques que tenías en Codes.html
+];
