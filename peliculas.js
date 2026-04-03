b5a73644828aa9f8bdcb0488b1827a343f3f3c44
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-04-03 12:27:29 -0500

    Update peliculas.js

--- peliculas.js
+++ peliculas.js
@@ -88,6 +88,17 @@ const baseDeDatos = [
         actriz: "Rena Sayama",
         tags: "Idol & Celebrity"
     },
+	{ 
+        id: "259LUXU-1875", 
+        titulo: "I love licking your dick.", 
+        poster: "https://cdn001.imggle.net/webp/poster/01202026/1772173522118.webp", 
+        bg: "https://i.ibb.co/BK3QDYmP/259-LUXU-1875.jpg", 
+        video: "https://appcine.b-cdn.net/Codegg/259LUXU-1875.mp4.mp4", 
+        estudio: "Luxury Tv",
+        fecha: "Feb. 27, 2026",
+        actriz: "Desconocida",
+        tags: "Reducing Mosaic"
+    },
 	
 	
     // Sigue pegando aquí todos los bloques que tenías en Codes.html
